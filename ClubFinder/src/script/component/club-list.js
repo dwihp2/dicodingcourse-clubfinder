7af92d7c96c05065a34df8ab3b801cfd5c777400
Dwi Heryanto <m.dwiheryanto2@gmail.com>
2020-04-16 17:33:16 +0700

    Add shadow DOM to ClubFinder customElement

--- ClubFinder/src/script/component/club-list.js
+++ ClubFinder/src/script/component/club-list.js
@@ -1,23 +1,53 @@
 import'./club-item.js';
 
 class ClubList extends HTMLElement {
+    constructor() {
+        super();
+        this.shadowDOM = this.attachShadow({mode: "open"});
+    }
+
     set clubs(clubs) {
         this._clubs = clubs;
         this.render();
     }
 
     render() {
-        this.innerHTML = "";
+        this.shadowDOM.innerHTML = `
+            <style>
+                * {
+                    padding: 0;
+                    margin: 0;
+                    box-sizing: border-box;
+                }
+                club-list {
+                    display: block;
+                    margin-top: 32px;
+                    width: 100%;
+                    padding: 16px;
+                }                            
+            </style>
+        `;
         this._clubs.forEach(club => {
             const clubItemElement = document.createElement("club-item");
             clubItemElement.club = club;
-            this.appendChild(clubItemElement);
+            this.shadowDOM.appendChild(clubItemElement);
         })
     }
 
     renderError(message) {
-        this.innerHTML = "";
-        this.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+        this.shadowDOM.innerHTML = `
+            <style>            
+                .placeholder {
+                    font-weight: lighter;
+                    color: rgba(0,0,0,0.5);
+                    -webkit-user-select: none;
+                    -moz-user-select: none;
+                    -ms-user-select: none;
+                    user-select: none;
+                }
+            </style>
+        `;
+        this.shadowDOM.innerHTML += `<h2 class="placeholder">${message}</h2>`;
     }
 }
 
